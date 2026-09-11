12c0b0b5a7a32f31a571d35afbc630d7fa831db8
Pluffyrain <henriquematheus151504@gmail.com>
2026-09-11 19:00:02 -0300

    Update PTTButton.hpp

--- src/PTTButton.hpp
+++ src/PTTButton.hpp
@@ -32,7 +32,6 @@ private:
     bool m_dragging = false;
 
     void updateVisual();
-    void checkConnection(float dt);
     void startTalking();
     void stopTalking();
     void triggerVoiceKey(bool down);
