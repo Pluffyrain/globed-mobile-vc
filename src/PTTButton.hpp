872ca1dee7d9180a9319b1cce54a0b94b1b0b830
Pluffyrain <henriquematheus151504@gmail.com>
2026-09-11 18:19:36 -0300

    Create PTTButton.hpp

---
+++ src/PTTButton.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+// A small circular touch button, shown bottom-left by default, that:
+//  - can be dragged to a new spot while "Move Button" is on in settings
+//  - starts/stops Globed voice chat while held (mirrors the desktop
+//    "Voice Chat Activate" keybind)
+//  - requests microphone permission the first time it's pressed
+//
+// Only compiled on touchscreen platforms -- see the #if guard in the .cpp.
+class PTTButton : public CCNode, public CCTargetedTouchDelegate {
+public:
+    static PTTButton* create();
+
+protected:
+    bool init() override;
+    void onEnter() override;
+    void onExit() override;
+
+    bool ccTouchBegan(CCTouch* touch, CCEvent* event) override;
+    void ccTouchMoved(CCTouch* touch, CCEvent* event) override;
+    void ccTouchEnded(CCTouch* touch, CCEvent* event) override;
+    void ccTouchCancelled(CCTouch* touch, CCEvent* event) override;
+
+private:
+    CCDrawNode* m_circle = nullptr;
+    CCLabelBMFont* m_label = nullptr;
+    bool m_holding = false;
+    bool m_dragging = false;
+
+    void updateVisual();
+    void checkConnection(float dt);
+    void startTalking();
+    void stopTalking();
+    void triggerVoiceKey(bool down);
+    void loadSavedPosition();
+    void savePosition();
+};
