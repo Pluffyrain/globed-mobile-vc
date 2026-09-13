d43bcb5073a25f5717a75bde1ef9304d99aee62d
Pluffyrain <henriquematheus151504@gmail.com>
2026-09-12 22:54:28 -0300

    Update PTTButton.hpp

--- src/PTTButton.hpp
+++ src/PTTButton.hpp
@@ -4,12 +4,7 @@
 
 using namespace geode::prelude;
 
-// A small circlebutton. Only appears whhen
-//  - the enable Button setting is on
-//  - youre connected to globed level dude typing in github is hard ad shit
-//.
-// NOTE: talking only actually transmits audio on a patched Globed build
-// with mobile voice sending enabled -- see this mod's description.
+// note ts button only works if you download my patched (see forks or something) globed file 
 class PTTButton : public CCNode, public CCTargetedTouchDelegate {
 public:
     static PTTButton* create();
